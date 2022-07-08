2aea72afebfd7efca1c89af0dd14ce9404c03007
saulgiedr1 <saulgiedr@gmail.com>
2022-07-08 10:47:43 +0300

    Pradzia

---
+++ src/PagrindinePrograma.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+
+public class PagrindinePrograma {
+    public static void main(String[] args) {
+        Scanner skaitytuvas = new Scanner(System.in);
+        ArrayList<Preke> prekes = nuskaitoPrekesIsFailo("prekes.csv");
+
+        isvestiListaAtskiraiEilutemis(prekes);
+
+        System.out.println("----------------------------------- " );
+
+        ArrayList<Preke> esanciosPrekes = rastiPrekesSandelyje(prekes);
+        isvestiListaAtskiraiEilutemis(esanciosPrekes);
+
+
+
+    }
+    public static ArrayList<Preke> nuskaitoPrekesIsFailo(String nuskaitomasFailas) {
+        ArrayList<Preke> prekes = new ArrayList<>();
+        try {
+            File failoObjektas = new File(nuskaitomasFailas);
+            Scanner skaitytuvas = new Scanner(failoObjektas);
+            skaitytuvas.nextLine();
+            while (skaitytuvas.hasNextLine()) {
+                String eilute = skaitytuvas.nextLine();
+                String[] stulpeliai = eilute.split(",");
+                Preke laikinaPreke = new Preke(Integer.parseInt(stulpeliai[0]), stulpeliai[1], stulpeliai[2], Double.parseDouble(stulpeliai[3]), Integer.parseInt(stulpeliai[4]), stulpeliai[5], stulpeliai[6]);
+                prekes.add(laikinaPreke);
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Nėra tokio failo.");
+        }
+        return prekes;
+    }
+
+    public static void isvestiListaAtskiraiEilutemis(ArrayList<Preke> prekes) {
+        for (Preke preke : prekes) {
+            System.out.println("preke = " + preke);
+        }
+    }
+    public static ArrayList<Preke> rastiPrekesSandelyje(ArrayList<Preke> visosPrekes) {
+        ArrayList<Preke> esanciosPrekes = new ArrayList<>();
+        for (Preke preke : visosPrekes) {
+            if (preke.arYraSandelyje()) {
+                esanciosPrekes.add(preke);
+            }
+        }
+        return esanciosPrekes;
+    }
+}
---
+++ src/Preke.java
@@ -0,0 +1,103 @@
+public class Preke {
+
+    private int id;
+    private String pavadinimas;
+    private String aprasymas;
+    private double kaina;
+    private int kiekis;
+    private String kategorija;
+    private String medziaga;
+
+    //konstruktoriai;
+    public Preke() {
+    }
+
+    public Preke(int id, String pavadinimas, String aprasymas, double kaina, int kiekis, String kategorija, String medziaga) {
+        this.id = id;
+        this.pavadinimas = pavadinimas;
+        this.aprasymas = aprasymas;
+        this.kaina = kaina;
+        this.kiekis = kiekis;
+        this.kategorija = kategorija;
+        this.medziaga = medziaga;
+    }
+    //metodai;
+    public boolean arYraSandelyje(){
+        return kiekis > 0;
+    }
+
+
+    //geteriai seteriai;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPavadinimas() {
+        return pavadinimas;
+    }
+
+    public void setPavadinimas(String pavadinimas) {
+        this.pavadinimas = pavadinimas;
+    }
+
+    public String getAprasymas() {
+        return aprasymas;
+    }
+
+    public void setAprasymas(String aprasymas) {
+        this.aprasymas = aprasymas;
+    }
+
+    public double getKaina() {
+        return kaina;
+    }
+
+    public void setKaina(double kaina) {
+        this.kaina = kaina;
+    }
+
+    public int getKiekis() {
+        return kiekis;
+    }
+
+    public void setKiekis(int kiekis) {
+        this.kiekis = kiekis;
+    }
+
+    public String getKategorija() {
+        return kategorija;
+    }
+
+    public void setKategorija(String kategorija) {
+        this.kategorija = kategorija;
+    }
+
+    public String getMedziaga() {
+        return medziaga;
+    }
+
+    public void setMedziaga(String medziaga) {
+        this.medziaga = medziaga;
+    }
+
+    //string;
+
+    @Override
+    public String toString() {
+        return "Preke{" +
+                "id=" + id +
+                ", pavadinimas='" + pavadinimas + '\'' +
+                ", aprasymas='" + aprasymas + '\'' +
+                ", kaina=" + kaina +
+                ", kiekis=" + kiekis +
+                ", kategorija='" + kategorija + '\'' +
+                ", medziaga='" + medziaga + '\'' +
+                '}';
+    }
+}
