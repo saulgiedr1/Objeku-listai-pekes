fbd8e1dce2b2e8f928c2027a5bb93e561e571a46
saulgiedr1 <saulgiedr@gmail.com>
2022-07-08 13:04:55 +0300

    Pradzia

--- src/PagrindinePrograma.java
+++ src/PagrindinePrograma.java
@@ -1,26 +1,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
-
-
 
 public class PagrindinePrograma {
     public static void main(String[] args) {
+
         Scanner skaitytuvas = new Scanner(System.in);
         ArrayList<Preke> prekes = nuskaitoPrekesIsFailo("prekes.csv");
 
         isvestiListaAtskiraiEilutemis(prekes);
 
-        System.out.println("----------------------------------- " );
+        System.out.println("----------------------------------- ");
 
         ArrayList<Preke> esanciosPrekes = rastiPrekesSandelyje(prekes);
         isvestiListaAtskiraiEilutemis(esanciosPrekes);
 
+        System.out.println("----------------------------------- ");
+
+        System.out.println("Prekiu kiekis sandelyje = " + prekiuKiekisSandelyje(prekes));
+
+        System.out.println("----------------------------------- ");
+
+        System.out.println("Brangiausia preke  = " + brangiausiaPreke(prekes));
 
 
     }
+
     public static ArrayList<Preke> nuskaitoPrekesIsFailo(String nuskaitomasFailas) {
         ArrayList<Preke> prekes = new ArrayList<>();
         try {
@@ -33,8 +40,7 @@ public class PagrindinePrograma {
                 Preke laikinaPreke = new Preke(Integer.parseInt(stulpeliai[0]), stulpeliai[1], stulpeliai[2], Double.parseDouble(stulpeliai[3]), Integer.parseInt(stulpeliai[4]), stulpeliai[5], stulpeliai[6]);
                 prekes.add(laikinaPreke);
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("Nėra tokio failo.");
         }
@@ -46,6 +52,7 @@ public class PagrindinePrograma {
             System.out.println("preke = " + preke);
         }
     }
+
     public static ArrayList<Preke> rastiPrekesSandelyje(ArrayList<Preke> visosPrekes) {
         ArrayList<Preke> esanciosPrekes = new ArrayList<>();
         for (Preke preke : visosPrekes) {
@@ -55,4 +62,36 @@ public class PagrindinePrograma {
         }
         return esanciosPrekes;
     }
+
+    public static double prekiuKiekisSandelyje(List<Preke> prekes) {
+        double kiekis = 0;
+        for (Preke preke : prekes) {
+            kiekis += preke.getKiekis();
+        }
+        return kiekis;
+    }
+
+    public static double brangiausiaPreke(List<Preke> prekes) {
+        double laikinasMax = 0 ;
+        for (Preke preke : prekes){
+            if(laikinasMax < preke.getKaina()){
+                laikinasMax = preke.getKaina();
+            }
+        }
+        return  laikinasMax;
+    }
+
+    public static ArrayList<Preke> brangiausiaPrekeSarase(List<Preke> prekes) {
+        ArrayList<Preke> brangiausia = new ArrayList<>() ;
+        for (Preke preke : prekes){
+            if(brangiausiaPreke()){
+              brangiausia.add(preke);
+           }
+        }
+        return  brangiausia;
+    }
+
+
+
+
 }
